c09ba514aba03f81ce62dcbc1847aabdaa5a2216
MacLane Wilkison <mwilkison@gmail.com>
2015-02-03 21:20:55 -0800

    Add assetsList helpers and dummy data

---
+++ client/templates/assets/assets_list.js
@@ -0,0 +1,18 @@
+var assetsData = [
+  {
+    title: 'Cession name 1',
+    url: 'http://google.com/'
+  },
+  {
+    title: 'Cession name 2',
+    url: 'http://google.com/'
+  },
+  {
+    title: 'Cession name 3',
+    url: 'http://google.com/'
+  }
+];
+
+Template.assetsList.helpers({
+  assets: assetsData
+});
