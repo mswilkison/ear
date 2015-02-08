3ce9cdbf2688ce5b3720fbf03d8dd129627cf230
MacLane Wilkison <mwilkison@gmail.com>
2015-02-07 21:58:47 -0800

    Upload files to s3 for cession submit

--- client/templates/cessions/cession_submit.js
+++ client/templates/cessions/cession_submit.js
@@ -1,3 +1,5 @@
+var uploader = new Slingshot.Upload("cessionDocumentUploads");
+
 Template.cessionSubmit.rendered = function() {
   $('#cessionDate').pickadate({
     format: 'mm/dd/yyyy'
@@ -14,7 +16,7 @@ Template.cessionSubmit.events({
       cessionType: $(e.target).find('[name=cessionType]').val(),
       cessionDate: $(e.target).find('[name=cessionDate]').val(),
       amountAgainst: $(e.target).find('[name=amountAgainst]').val(),
-      inputFile: $(e.target).find('[name=inputFile]').val(),
+      inputFile: $(e.target).find('[name=inputFile]')[0].files[0],
       assetDescription: $(e.target).find('[name=assetDescription]').val(),
       cessionStatus: "Active"
     };
@@ -29,6 +31,13 @@ Template.cessionSubmit.events({
       if (error) {
         return throwError(error.reason);
       }
+      if (cession.inputFile) {
+        uploader.send(cession.inputFile, function(error, downloadUrl) {
+          console.log(error);
+          Cessions.update(result._id, {$set: {"inputFile": downloadUrl}});
+        });
+      }
+
       Router.go('cessionPage', {_id: result._id});
     });
   }
--- server/uploads.js
+++ server/uploads.js
@@ -14,7 +14,28 @@ Slingshot.createDirective("profilePictureUploads", Slingshot.S3Storage, {
 
   key: function(file) {
     // Store file into a directory by the user's id
-    var directory = this.userId + "/" + file.name;
+    var directory = "users/" + this.userId + "/" + file.name;
+    return directory;
+  }
+});
+
+Slingshot.createDirective("cessionDocumentUploads", Slingshot.S3Storage, {
+  bucket: "electronic-asset-registry",
+
+  acl: "public-read",
+
+  authorize: function() {
+    // Deny upload if user is not logged in
+    if (!this.userId) {
+      var message = "Please login before uploading files";
+      throw new Meteor.Error("Login Required", message);
+    }
+    return true;
+  },
+
+  key: function(file) {
+    // Store file into a directory by the user's id
+    var directory = "cessions/" + this.userId + "/" + file.name;
     return directory;
   }
 });
